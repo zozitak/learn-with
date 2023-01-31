333d9a4fe548d964d1561d43955747e713c624b7
Takacs Zoltan <zozitak@gmail.com>
2023-01-31 20:17:02 +0100

    fix: windows path

--- 2023_01_31/base_api.py
+++ 2023_01_31/base_api.py
@@ -4,6 +4,7 @@
 
 import json
 from flask import Flask, request, jsonify
+import os
 
 app = Flask(__name__)
 
@@ -13,7 +14,7 @@ def query_records():
     print('------------------------------------------------------------------------')
     name = request.args.get('name')
     print(name)
-    with open('E:\\programing\\learn\\2023_01_31\\tmp\\data.txt', 'r') as f:
+    with open(os.getcwd() + '\\2023_01_31\\tmp\\data.txt', 'r') as f:
         data = f.read()
         records = json.loads(data)
         print(records)
@@ -27,14 +28,14 @@ def query_records():
 @app.route('/', methods=['PUT'])
 def create_record():
     record = json.loads(request.data)
-    with open('/tmp/data.txt', 'r') as f:
+    with open(os.getcwd() + '\\2023_01_31\\tmp\\data.txt', 'r') as f:
         data = f.read()
     if not data:
         records = [record]
     else:
         records = json.loads(data)
         records.append(record)
-    with open('/tmp/data.txt', 'w') as f:
+    with open(os.getcwd() + '\\2023_01_31\\tmp\\data.txt', 'w') as f:
         f.write(json.dumps(records, indent=2))
     return jsonify(record)
 
@@ -43,14 +44,14 @@ def create_record():
 def update_record():
     record = json.loads(request.data)
     new_records = []
-    with open('/tmp/data.txt', 'r') as f:
+    with open(os.getcwd() + '\\2023_01_31\\tmp\\data.txt', 'r') as f:
         data = f.read()
         records = json.loads(data)
     for r in records:
         if r['name'] == record['name']:
             r['email'] = record['email']
         new_records.append(r)
-    with open('/tmp/data.txt', 'w') as f:
+    with open(os.getcwd() + '\\2023_01_31\\tmp\\data.txt', 'w') as f:
         f.write(json.dumps(new_records, indent=2))
     return jsonify(record)
 
@@ -59,14 +60,14 @@ def update_record():
 def delte_record():
     record = json.loads(request.data)
     new_records = []
-    with open('/tmp/data.txt', 'r') as f:
+    with open(os.getcwd() + '\\2023_01_31\\tmp\\data.txt', 'r') as f:
         data = f.read()
         records = json.loads(data)
         for r in records:
             if r['name'] == record['name']:
                 continue
             new_records.append(r)
-    with open('/tmp/data.txt', 'w') as f:
+    with open(os.getcwd() + '\\2023_01_31\\tmp\\data.txt', 'w') as f:
         f.write(json.dumps(new_records, indent=2))
     return jsonify(record)
 
